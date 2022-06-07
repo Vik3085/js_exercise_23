4b9cbd72282be719fb8704f02cb226a3fc0aaad4
Vik3085 <102584827+Vik3085@users.noreply.github.com>
2022-06-07 10:56:42 +0200

    Add files via upload

---
+++ exercise-1.js
@@ -0,0 +1,15 @@
+const person = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+console.log("--------- Solo le chiavi ---------");
+
+console.log(Object.keys(person));
+
+console.log("--------- Chiavi e Valori ---------");
+
+console.log(Object.entries(person));
+
+// Print values of person using Object.keys
